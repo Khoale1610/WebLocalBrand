73c069aeb4564e6771f02abb343ceb179ec287ea
Hannah <tuyen10162006@gmail.com>
2026-08-31 14:04:19 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,10 @@ Route::get('/search', [HomeController::class, 'search'])->name('products.search'
 // 2. Chi tiết sản phẩm
 Route::get('/san-pham/{id}', [ProductController::class, 'show'])->name('products.show');
 
-// 3. Quản lý Giỏ hàng (Cart)
+//3. Xử lý trang danh mục
+Route::get('/danh-muc/{slug}', [CategoryController::class, 'show'])->name('category.show');
+
+// 4. Quản lý Giỏ hàng (Cart)
 Route::prefix('cart')->name('cart.')->group(function () {
     Route::get('/', [CartController::class, 'index'])->name('index');
     Route::post('/add', [CartController::class, 'add'])->name('add');
